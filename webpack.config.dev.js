45d68f85f2bcb732195a2e6b271cbf6d4f493913
Mohamed El Amine DADDOU <daddou.ma@outlook.com>
2018-08-15 16:15:42 +0200

    initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundling.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader', 'eslint-loader'],
+    }],
+  },
+  plugins: [
+  ],
+  devtool: 'cheap-source-map',
+  mode: 'development',
+};
